e097d6eb19fd0e15fa35dc6d201c1a37e8b164dc
Dmitry Glavchev <dmitry.glavchev@gmail.com>
2025-03-15 23:21:06 +0300

    lets deploy smt

---
+++ vue-clicker-app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+</script>
+
+<template>
+	<HomeView></HomeView>
+</template>
+
+<style scoped>
+</style>
---
+++ vue-clicker-app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { Alert } from 'vue-tg'
+</script>
+
+<template>
+  <main>
+    Main Page
+    <Alert message="Hello Tg from Mini App !" />
+  </main>
+</template>
